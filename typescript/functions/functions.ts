bcae0463cead808744ab5cdede1f64c2ef5a9468
Michał Osiatycki <mosiatycki@Gispartner.pl>
2018-10-16 10:36:41 +0200

    clear

--- typescript/functions/functions.ts
+++ typescript/functions/functions.ts
@@ -1,34 +1,3 @@
-class ExampleFunctions {
-  msg: string = "HelloWorld";
-
-  showMsg(): void {
-    console.log(this.msg);
-  }
-
-  hello(): string {
-    return "Hello world, functions return type string";
-  }
-
-  helloArg(message: string) {
-    console.log(message);
-  }
-
-  helloManyArgLastOptional(message: string, message2?: string) {
-    console.log(message + " " + message2);
-  }
-
-  helloManyArgLastOptionalSet(message: string, message2 = "HelloWorld") {
-    console.log(message + " " + message2);
-  }
-
-  helloArrowFunction = message =>
-    console.log("Hello world arrow function" + message);
-
-  helloArrowFunction2 = () => console.log("ArrowFunction");
-
-  helloArrowFunction3 = (message: string) => console.log(message);
-}
-
 var example = new ExampleFunctions();
 example.showMsg();
 example.hello();
